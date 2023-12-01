5857323fb182a9ba755d20bb4266555846bba811
sarwesh123 <20ee01047@iitbbs.ac.in>
2023-12-01 20:27:09 +0530

    final commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 //We are using common js module not es6 module
 const mongoose=require('mongoose');
 const express=require('express');
-const mongoURI="mongodb://localhost:27017/inotebook"
+const dotenv=require('dotenv');
+const mongoURI=`mongodb+srv://sarwesh123:dqcV6VADFa7ii9Lc@cluster0.g9wghhl.mongodb.net/`
+
+
 const connectToMongo=async()=>{
     try{
     mongoose.connect(mongoURI)
--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,38 @@ var cors = require('cors')
 app.use(cors())
 //Middleware if client send some req to server
 app.use(express.json())
+const dotenv=require('dotenv');
 
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes.js'))
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
+const path = require('path')
+dotenv.config();
+// const PORT= process.env.PORT;
+// console.log(PORT);
+
+const __dirname1=path.resolve();
+const ab=path.join(__dirname1,"/../build")
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(ab));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(ab,"index.html"));
+});
+}
+else{
+    app.get("/",(req,res)=>{
+        res.send("api working");
+});
+}
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello World");
+// })
 
 app.listen(port,()=>{
  console.log('Example app listening at http://localhost:' + port)
+ console.log(ab)
 })
